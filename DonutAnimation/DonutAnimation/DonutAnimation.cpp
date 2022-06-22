80b900b8d365ba14c6610ba50069c274189199ef
reAuthorName <liubomyrcherevatyy@gmail.com>
2022-06-22 14:39:31 +0300

    Finished

--- DonutAnimation/DonutAnimation/DonutAnimation.cpp
+++ DonutAnimation/DonutAnimation/DonutAnimation.cpp
@@ -1,20 +1,54 @@
-// DonutAnimation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#pragma region LIBS
 
 #include <iostream>
+#include <string>
+#pragma endregion
+
+#pragma region VARS
+int k;
+
+const int size = 1760;
+const float spinningSpeed = .09;
+#pragma endregion
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A = 0, B = 0, i, j, z[size];
+	char b[size];
+	printf("\n1b[2J");
+	for (;;)
+	{
+		memset(b, 32, size);
+		memset(z, 0, 7040);
+
+		for (j = 0; j < 6.28; j += spinningSpeed)
+		{
+			for (i = 0; i < 6.28; i += spinningSpeed)
+			{
+				float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
+					h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i), m = cos(B),
+					n = sin(B), t = c * h * g - f * e;
+
+				int x = 40 + 30 * D * (l * h * m - t * n);
+				int y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y,
+					N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+				if (22 > y && y > 0 && 80 > x && x > 0 && D > z[0])
+				{
+					z[o] = D;
+					b[o] = ".,-~:;=!*#$@:"[N > 0 ? N : 0];
+				}
+			}		
+		}
+
+		printf("\x1b[H");
+		for (k = 0; k < size + 1; k++)
+			putchar(k % 80 ? b[k] : 10);
+
+		A += .04;
+		B += .02;
+	}
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
